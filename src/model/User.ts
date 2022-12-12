9ea35ba8c9f674598055de68468ffeca8b46b7f4
kuba <adamiak.dev@gmail.com>
2022-12-12 14:10:27 +0100

    ccc

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,3 +1,21 @@
 export interface User {
     id: number;
+    firstName: string,
+    lastName: string,
+    address: Address
+    // {
+    //     "street": "kolorowa",
+    //     "street_number": "9",
+    //     "city": "Poznan",
+    //     "zip_code": "60-999",
+    //     "phone_number": "555-666-777"
+    //   }
+}
+
+class Address {
+    street: string | undefined;
+    street_number: string | undefined;
+    city: string | undefined;
+    zip_code: string | undefined;
+    phone_number: string | undefined;
 }
